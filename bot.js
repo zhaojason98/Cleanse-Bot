1a3113bd4f8080c0878fc0d274da76739d568fe2
Jason Zhao <zhaojason98@gmail.com>
2018-11-11 13:32:02 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,4 +31,26 @@ bot.on('guildMemberRemove', member => {
 	}
 });
 
+// Create an event listener for new guild members
+bot.on('guildMemberAdd', member => {
+	member.guild.channels.find("id", '398691402139172864').send('Hi <@!' + member.user.id + ">");
+	member.guild.channels.find("id", '398691402139172864').send({
+		embed: {
+			color: 3447003,
+			fields: [{
+				name: "Welcome to **Cleanse**!",
+				value: "Please tag one of the Mistress or Royalty members your IGN information and which MapleStory game you came from! Cleanse is a guild in MS1 & 2 so please be careful who you ping!"
+			}],
+		}
+	}); 
+});
+
+bot.on('guildMemberRemove', member => {
+	if (member.nickname) {
+   		member.guild.channels.find("id", '413609953077624832').send( member.toString() + ' (' + member.user.username + '/' + member.nickname + ') has left the server');
+	} else {
+		member.guild.channels.find("id", '413609953077624832').send( member.toString() + ' (' + member.user.username + ') has left the server');
+	}
+});
+
 bot.login(process.env.BOT_TOKEN);
